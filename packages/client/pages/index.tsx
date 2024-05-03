7c9fca74d0bd2a1c55d215b82e9cd2fc9d473151
objStevo <hschalco@gmail.com>
2024-05-03 11:07:21 -0700

    initial commit

---
+++ packages/client/pages/index.tsx
@@ -0,0 +1,16 @@
+import { Provider } from "react-redux";
+import { store } from "../store";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { themeOptions } from "../components/theme";
+
+const Home: any = () => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={themeOptions}>
+        <h1>Welcome</h1>
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+export default Home;
