c88daf6313351d1d5392e90b476e7b05ca7528ff
objStevo <hschalco@gmail.com>
2024-07-09 21:32:14 -0700

    clear unneeded imports

--- src/app/sessions/[variant]/page.tsx
+++ src/app/sessions/[variant]/page.tsx
@@ -1,16 +1,4 @@
 "use client";
-import AdyenState from "@/components/custom/adyen/AdyenState";
-import API from "@/components/custom/sandbox/backend/API";
-import CSS from "@/components/custom/sandbox/frontend/CSS";
-import HTML from "@/components/custom/sandbox/frontend/HTML";
-import JS from "@/components/custom/sandbox/frontend/JS";
-import Events from "@/components/custom/sandbox/webhooks/Events";
-import SandboxLayout from "@/components/custom/sandbox/layout";
-import Sidebar from "@/components/custom/sandbox/layout/sidebar";
-import TabbedMain from "@/components/custom/sandbox/layout/tabbedMain";
-import Topbar from "@/components/custom/sandbox/layout/topbar";
-
-import { useState } from "react";
 
 const Page: any = () => {
   return <h1>sessions</h1>;
