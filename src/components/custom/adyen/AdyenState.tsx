3274b80a99f6eff9302c3a87e199d6c550430780
objStevo <hschalco@gmail.com>
2024-07-12 20:22:42 -0700

    code-editor

--- src/components/custom/adyen/AdyenState.tsx
+++ src/components/custom/adyen/AdyenState.tsx
@@ -1,20 +1,48 @@
+import React, { useEffect, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { githubLight } from "@uiw/codemirror-theme-github";
 import { useSelector } from "react-redux";
 import type { RootState } from "@/store/store";
 import { EditorView } from "@codemirror/view";
+import * as prettier from "prettier/standalone";
+import * as parserBabel from "prettier/parser-babel";
+import * as prettierPluginEstree from "prettier/plugins/estree";
 
 const AdyenState = () => {
   const { variantState } = useSelector(
     (state: RootState) => state.adyenVariant
   );
+  const [formattedCode, setFormattedCode] = useState<string>("");
+
+  const prettify = async (json: any): Promise<string> => {
+    try {
+      const prettierVersion = prettier.format(JSON.stringify(json), {
+        parser: "json",
+        plugins: [parserBabel, prettierPluginEstree],
+        tabWidth: 4,
+        useTabs: false,
+      });
+      return prettierVersion;
+    } catch (error) {
+      console.error("Prettier formatting error: ", error);
+      return JSON.stringify(json, null, 4); // Fallback to basic formatting
+    }
+  };
+
+  useEffect(() => {
+    const formatCode = async () => {
+      const formatted = await prettify(variantState);
+      setFormattedCode(formatted);
+    };
+    formatCode();
+  }, [variantState]);
 
   return (
     <div className="flex">
-      <div className="flex codemirror-wrapper ">
+      <div className="flex codemirror-wrapper pb-5">
         <CodeMirror
-          value={JSON.stringify(variantState)}
+          value={formattedCode}
           height="100%"
           theme={githubLight}
           extensions={[javascript({ jsx: true }), EditorView.lineWrapping]}
@@ -23,4 +51,5 @@ const AdyenState = () => {
     </div>
   );
 };
+
 export default AdyenState;
