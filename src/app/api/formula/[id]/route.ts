d08685190c5ce99971de1310f54c9ecf27806c4e
Mustafa <gomushi@gmail.com>
2024-08-14 21:21:56 -0500

    delete success

--- src/app/api/formula/[id]/route.ts
+++ src/app/api/formula/[id]/route.ts
@@ -1,3 +1,5 @@
+import dbConnect from "@/lib/db";
+import Formula from "@/schema/Formula";
 import { NextRequest } from "next/server";
 
 interface Params {
@@ -10,5 +12,17 @@ export async function GET(request: NextRequest, { params }: Params) {
   const { id } = params;
 
   console.log(`Getting a Formula with id: ${id}`);
+  return Response.json({ id: id });
+}
+
+export async function DELETE(request: NextRequest, { params }: Params) {
+  const { id } = params;
+
+  console.log(`Proceeding to delete formula with id ${id}`);
+
+  await dbConnect();
+
+  let result = await Formula.findByIdAndDelete(id);
+  console.log(result);
   return Response.json({ id: id });
 }
