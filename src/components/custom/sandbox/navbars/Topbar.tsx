2955674b021cdf8438cd69e7b8620f45e3ede8ef
objStevo <hschalco@gmail.com>
2024-09-08 22:15:13 -0700

    ugh

--- src/components/custom/sandbox/navbars/Topbar.tsx
+++ src/components/custom/sandbox/navbars/Topbar.tsx
@@ -15,7 +15,6 @@ const Topbar = (props: any) => {
   ).length;
 
   // Set the merchant account name in the top left corner, with the ability to edit the value. This will change the merchant account across all of the API requests. Although the user can always change it later. If the merchanat accounts are different than what is set then we throw a warning
-  console.log('unsavedChanges', unsavedChanges);
   return (
     <span
       className="absolute top-0 left-[var(--sidebar-width)] h-[var(--topbar-width)] border-b-2 flex items-center justify-end pr-2"
