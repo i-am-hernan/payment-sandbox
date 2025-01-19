237d5fe3d977e6c55f3388c487f4e69612d396ba
objStevo <hschalco@gmail.com>
2025-01-18 16:31:11 -0800

    touch up footer

--- src/components/custom/sandbox/navbars/FooterBar.tsx
+++ src/components/custom/sandbox/navbars/FooterBar.tsx
@@ -3,13 +3,20 @@ import { useSelector } from "react-redux";
 import ScienceIcon from "@mui/icons-material/Science";
 import TitleIcon from "@mui/icons-material/Title";
 import BuildIcon from "@mui/icons-material/Build";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 
 const FooterBar = (props: any) => {
   const { integration } = props;
+  const company = process.env.NEXT_PUBLIC_COMPANY_NAME;
   const { variantName, title } = useSelector(
     (state: RootState) => state.sandbox
   );
   const footerButtons = [
+    {
+      name: "Company",
+      icon: <StorefrontIcon className="!text-foreground !text-[1rem]" />,
+      message: `${company ? company : "default"}`,
+    },
     {
       name: "Integration",
       icon: <BuildIcon className="!text-foreground !text-[1rem]" />,
@@ -28,13 +35,13 @@ const FooterBar = (props: any) => {
   ];
   return (
     <span
-      className="absolute bottom-0 border-t-2 left-[var(--sidebar-width)] h-[var(--footerbar-width)] flex px-2"
+      className="absolute bottom-0 border-t-2 left-[var(--sidebar-width)] h-[var(--footerbar-width)] flex justify-end px-2"
       style={{ width: `calc(100vw - var(--sidebar-width))` }}
     >
       {footerButtons.map((button, index) => (
         <div
           key={index}
-          className="flex h-[100%] items-end max-w-[15rem] py-[0.2rem] pl-4"
+          className="flex h-[100%] items-end max-w-[15rem] py-[0.2rem] pr-5"
         >
           {button.icon}
           <p className="text-[0.6rem] text-foreground pl-[0.1rem] ">
