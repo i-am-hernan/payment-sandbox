040c9f0a4dd444aa76296548681dc42205afd2ee
objStevo <hschalco@gmail.com>
2025-01-19 21:45:36 -0800

    add cors

--- next.config.js
+++ next.config.js
@@ -6,6 +6,27 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: false, // Set to true to ignore TypeScript errors during build
   },
+  headers: async () => {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: false,
   // Custom webpack configuration
   webpack: (config, { isServer }) => {
