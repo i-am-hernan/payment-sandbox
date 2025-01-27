8880c5a93d8a4c5de123fb19268079bdf694d8e7
objStevo <hschalco@gmail.com>
2025-01-26 22:00:18 -0800

    fix empty style

--- src/schema/Formula.ts
+++ src/schema/Formula.ts
@@ -44,7 +44,12 @@ const FormulaSchema: Schema = new Schema(
         paymentsDetails: { type: Schema.Types.Mixed, required: true },
         sessions: { type: Schema.Types.Mixed, required: true },
       },
-      style: { type: String, required: true },
+      style: {
+        type: String,
+        required: function (this: Formula) {
+          return typeof this.configuration.style !== "string";
+        },
+      },
       txVariantConfiguration: { type: String, required: false },
       isRedirect: { type: Boolean, required: true },
     },
